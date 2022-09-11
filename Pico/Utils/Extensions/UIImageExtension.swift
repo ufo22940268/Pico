c997ed772f815eb11636e404a68f794bc1a7f9d8
Frank Cheng <ufo22940268@gmail.com>
2022-09-11 10:03:48 +0800

    fix wechat share. 

--- Pico/Utils/Extensions/UIImageExtension.swift
+++ Pico/Utils/Extensions/UIImageExtension.swift
@@ -11,17 +11,11 @@ import UIKit
 
 extension UIImage {
     func thumbnail() -> UIImage {
-//        let newSize = CGSize(width: 200, height: 200)
-//        let image = UIGraphicsImageRenderer(size: newSize).image { context in
-//            self.draw(in: CGRect(origin: CGPoint(x: 0, y: 0), size: newSize))
-//        }
-//        return image
-        
         let imageData = UIImagePNGRepresentation(self)!
         let options = [
             kCGImageSourceCreateThumbnailWithTransform: true,
             kCGImageSourceCreateThumbnailFromImageAlways: true,
-            kCGImageSourceThumbnailMaxPixelSize: 300] as CFDictionary
+            kCGImageSourceThumbnailMaxPixelSize: 600] as CFDictionary
         let source = CGImageSourceCreateWithData(imageData as CFData, nil)!
         let imageReference = CGImageSourceCreateThumbnailAtIndex(source, 0, options)!
         let thumbnail = UIImage(cgImage: imageReference)
